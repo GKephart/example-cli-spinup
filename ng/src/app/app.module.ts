8f3c9da96287c3902c37450f50f3761972ab5421
george <gkephart@cnm.edu>
2019-03-11 10:06:28 -0600

    added working example code

---
+++ ng/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import {allAppComponents, appRoutingProviders, routing} from "./app.routes"
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule , routing],
+  declarations: [ ...allAppComponents, AppComponent],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
